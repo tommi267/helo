48219b46f64fbaad9dc54e23de932ee25ba0e5ab
tommi267 <tommi.t.nguyen@icloud.com>
2020-06-17 20:40:45 -0400

    push

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
+import {logoutUser} from '../../ducks/reducer'
+import {withRouter} from 'react-router-dom'
 
 class Nav extends Component {
-    
+    logout = () => {
+        axios.post('/auth/logout')
+        .then(() => {
+            this.props.logoutUser()
+            this.props.history.push('/')
+        })
+    }
 
     render(props) {
         const{username,profilePic} = this.props.user
@@ -14,7 +23,9 @@ class Nav extends Component {
                 <p>{username}</p>
                 <Link to='/dashboard'>Home</Link>
                 <Link to='new'>New Post</Link>
-                <Link to='/'>Logout</Link>
+                <button type='submit'
+                        value='Logout'
+                        onClick={(e) => this.logout(e)}>Logout</button>
             </div>
         )
     }
@@ -22,4 +33,4 @@ class Nav extends Component {
 
 const mapStateToProps = reduxState => reduxState
 
-export default connect(mapStateToProps)(Nav)
+export default withRouter(connect(mapStateToProps,{logoutUser})(Nav))
